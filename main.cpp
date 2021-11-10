5c250e213fb114bbf81e641fff78149ca0b9c197
Rinsuki <25454024+rinsukiyo@users.noreply.github.com>
2021-11-10 15:56:21 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  CPT111
+//
+//  Created by Rinsuki on 2021/11/10.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "Hello, World!\n" << endl;
+    return 0;
+}
